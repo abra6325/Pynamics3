e63bbade9f297d816058942d57ce6eab84a6f2eb
BuildTools <unconfigured@null.spigotmc.org>
2024-08-26 21:34:01 +0800

    i failed

--- cpacks/tester.py
+++ cpacks/tester.py
@@ -1,8 +1,15 @@
-import cik_core
+import cynamics
 import faulthandler
 import time
 if __name__ == "__main__":
 
     faulthandler.enable() #start @ the beginning
+start = time.time()
+cnt = 0
+while time.time() - start <= 1:
+    cynamics.sleep(7812500,True)
+    cnt+=1
 
-cik_core.
+print(cnt)
+
+
