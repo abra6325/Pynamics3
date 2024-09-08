56d47448cc4130a7a0711ac8f54a6c88497cb739
BuildTools <unconfigured@null.spigotmc.org>
2024-09-08 21:31:59 +0800

    finished events

--- demo/test_scripting/init.py
+++ demo/test_scripting/init.py
@@ -1,6 +1,6 @@
 import yik
 
-yik.set_naming_seed(66777888) # makes sure all naming references are the same
+yik.set_naming_seed(66777888)  # makes sure all naming references are the same
 
 print("Create NULL")
 a = yik.ApplicationObject("test_engine")
@@ -8,22 +8,20 @@ a = yik.ApplicationObject("test_engine")
 print("Create GL")
 e = yik.EventBus(a)
 
+
 @e.event_subscriber(yik.EVENTS.ADD_CHILD)
-def test1(e):
+def test1(e: yik.AddChildEvent):
     print(e)
     e.success = True
-    print(e.parent)
-
-
 
 print("Created Event Bus")
 y = yik.WindowGLTk(a)
 print("Create WORLD")
 x = yik.World(y)
 x.launch()
-print(x.root.app_id)
 print("Create OBJECT")
 obj = yik.RenderableGameObject(x)
 x.attach_script("test_aa")
-#.show()
+
+print("SHOWING WORLDS")
 y.launch()
